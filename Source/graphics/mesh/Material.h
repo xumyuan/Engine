78975970b42e2c358dd48a00ab0f895d8b1ca29c
xumy <947656035@qq.com>
2026-03-05 20:57:45 +0800

    fix：修改默认粗糙度

--- Source/graphics/mesh/Material.h
+++ Source/graphics/mesh/Material.h
@@ -33,7 +33,7 @@ namespace engine {
 		Texture* m_AlbedoMap, * m_NormalMap, * m_MetallicMap, * m_RoughnessMap, * m_AmbientOcclusionMap, * m_EmissionMap;
 		float m_ParallaxStrength = 0.07f;
 		glm::vec4 m_AlbedoColour = glm::vec4(0.894f, 0.023f, 0.992f, 1.0f);
-		float m_MetallicValue = 0.0f, m_RoughnessValue = 0.0f;
+		float m_MetallicValue = 0.0f, m_RoughnessValue = 0.5f;
 
 		// Emission values
 		float m_EmissionIntensity = 1.0f;
